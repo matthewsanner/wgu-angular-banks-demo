84a82624f51ad5b285e0748609396a145f3be1bf
Matthew Sanner <matthewsannerdev@gmail.com>
2024-11-20 18:43:57 -0600

    updates root component and class title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'wgu-angular-banks-demo';
+  title = 'Investment Banks';
 }
