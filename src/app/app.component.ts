ea86d01bc07a851ea57baab93169a4c7b5e4da8e
Matthew Sanner <matthewsannerdev@gmail.com>
2024-11-21 19:35:40 -0600

    adds bank and bank detail components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { BanksComponent } from './banks/banks.component';
+// import { BankDetailComponent } from './bank-detail/bank-detail.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BanksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
